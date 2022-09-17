6414af94cd256b5b9772c89779418de9b5d4e3e4
/home/jason/.gitconfig <reddkingdom@csu.fullerton.edu>
2022-09-17 00:42:16 -0700

    added multithreading

--- parser.py
+++ parser.py
@@ -3,24 +3,32 @@
 import os
 import re
 import shutil
+import multiprocessing, threading
 from pathlib import Path
 
 
-path = Path('path/to/target/directory/')
+# path = Path('path/to/target/directory/')
+path = Path('submissions/')
 list_files = (fp for fp in os.listdir(path) if Path(fp).is_file())
 
+lock = multiprocessing.Lock()
 
-def parser():
-    os.chdir(path)
+def parse():
+    title = re.search(r'([a-z|A-Z]+)_.', os.path.splitext(name)[0])
+    new_name = title.group(1)
 
-    for name in list_files:
-        title = re.search(r'([a-z|A-Z]+)_.', os.path.splitext(name)[0])
-        new_name = title.group(1)
+    lock.acquire()
+    if not Path(new_name).is_dir():
+        Path(new_name).mkdir(parents=True, exist_ok=True)
+    lock.release()
 
-        if not Path(new_name).is_dir():
-            Path(new_name).mkdir(parents=True, exist_ok=True)
-        shutil.move(name, new_name)
+    shutil.move(name, new_name)
 
 
 if __name__ == '__main__':
-    parser()
+    os.chdir(path)
+
+    for tid, name in enumerate(list_files):
+        t = threading.Thread(target=parse, name=tid, args=[])
+        t.start()
+        t.join()
