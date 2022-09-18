ce9586b117fccec04ff87af6b343f2595c677cc8
/home/jason/.gitconfig <reddkingdom@csu.fullerton.edu>
2022-09-17 18:34:58 -0700

    new

--- parser.py
+++ parser.py
@@ -3,35 +3,25 @@
 import os
 import re
 import shutil
-import multiprocessing, threading
 from pathlib import Path
 
 
 path = Path('submissions/')
 list_files = (_ for _ in os.listdir(path) if Path(_).is_file())
 
-lock = multiprocessing.Lock()
 
-def parse(name):
-    """Synthesizes each submission's file name and
-    moves it to a new/existing folder
-    """
+def parser():
+    """Parses each submission file's name and moves it to a new/existing folder"""
+    os.chdir(path)
 
-    title = re.search(r'([a-z|A-Z]+)_.', os.path.splitext(name)[0])
-    new_name = title.group(1)
-
-    lock.acquire()  # enforce mutual-exclusion as condition should be autonomously checked
-    if not Path(new_name).is_dir():
-        Path(new_name).mkdir(parents=True, exist_ok=True)
-    lock.release()
-
-    shutil.move(name, new_name)
+    for fd in list_files:
+        name, ext = os.path.splitext(fd)
+        title = re.search(r'([a-zA-Z]+)_.', name)
+        rename = title.group(1)
+        if not Path(rename).is_dir():
+            Path(rename).mkdir(parents=True, exist_ok=True)
+        shutil.move(fd, rename+"/"+rename+ext)
 
 
 if __name__ == '__main__':
-    os.chdir(path)
-
-    for tid, name in enumerate(list_files):
-        t = threading.Thread(target=parse, name=tid, args=[name])
-        t.start()
-        t.join()
+    parser()
